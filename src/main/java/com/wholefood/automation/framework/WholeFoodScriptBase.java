b5c3798f874eaf717dee89ca6d770a8c3a1ea665
shaheensuman <jshaheen03@gmail.com>
2017-09-25 23:41:19 -0400

    one

--- src/main/java/com/wholefood/automation/framework/WholeFoodScriptBase.java
+++ src/main/java/com/wholefood/automation/framework/WholeFoodScriptBase.java
@@ -9,8 +9,12 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,12 +27,15 @@ public class WholeFoodScriptBase {
     public WebDriver driver = null;
     ApplicationController wholeFood = null;
 
-   // @Parameters("browser")
+    public static final String USERNAME = "jahangirshaheen1";
+    public static final String AUTOMATE_KEY = "Fa4hfLwzmSwVG9L4h2Qp";
+    public static final String REMOTE_HUB = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    @Parameters("browser")
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(String browser) throws MalformedURLException {
 
-   //    if (browser.equalsIgnoreCase("chrome")) {
+   /*    if (browser.equalsIgnoreCase("chrome")) {
             ChromeDriverManager.getInstance().setup();
             driver = new ChromeDriver();
 
@@ -52,14 +59,49 @@ public class WholeFoodScriptBase {
 
         System.out.println("*************before  test method*************");
 
-         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-         try {
+        if (browser.equalsIgnoreCase("ff-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "Firefox");
+            caps.setCapability("browser_version", "56.0 beta");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+            WebDriver driver = new RemoteWebDriver(new URL(REMOTE_HUB), caps);
+        }else if(browser.equalsIgnoreCase("ch-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "Chrome");
+            caps.setCapability("browser_version", "61.0");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+        }else if (browser.equalsIgnoreCase("ie-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "IE");
+            caps.setCapability("browser_version", "11.0");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+        }
+        if (browser.equalsIgnoreCase("chrome")){
+            ChromeDriverManager.getInstance().setup();
+            driver=new ChromeDriver();
+        }else if(browser.equalsIgnoreCase("FireFox")){
+           // FirefoxDriverManager.getInstance().setup();
+            String DriverPath = System.getProperty("user.dir");
+            String FFpath = DriverPath + "\\Drivers\\FF\\32bits\\geckodriver.exe";
+            System.setProperty("webdriver.gecko.driver", FFpath);
+            driver = new FirefoxDriver();
+
+        }
+
+
+        try {
              driver.navigate().to("https://www.wholefoodsmarket.com/");
          }catch (Exception ex){
              ex.printStackTrace();
          }
-
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
          driver.manage().window().maximize();
 
 
@@ -79,7 +121,12 @@ public class WholeFoodScriptBase {
 
        if (driver != null) {
             driver.close();
-           // driver.quit();
+            try {
+                driver.quit();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+
             wholeFood=null;
         }
 
---
+++ src/main/java/com/wholefood/automation/framework/WholeFoodsRemoteExecution.java
@@ -0,0 +1,81 @@
+package com.wholefood.automation.framework;
+
+import com.wholefood.automation.framework.controller.ApplicationController;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by jahangir shaheen on 9/25/2017.
+ */
+public class WholeFoodsRemoteExecution {
+    public WebDriver driver=null;
+    public  ApplicationController wholeFood=null;
+    public static final String USERNAME = "jahangirshaheen1";
+    public static final String AUTOMATE_KEY = "Fa4hfLwzmSwVG9L4h2Qp";
+    public static final String REMOTE_HUB = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    @Parameters("browser")
+    @BeforeMethod
+    public void setUp(String browser) throws MalformedURLException {
+        if (browser.equalsIgnoreCase("ff-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "Firefox");
+            caps.setCapability("browser_version", "56.0 beta");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+            WebDriver driver = new RemoteWebDriver(new URL(REMOTE_HUB), caps);
+        }else if(browser.equalsIgnoreCase("ch-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "Chrome");
+            caps.setCapability("browser_version", "61.0");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+        }else if (browser.equalsIgnoreCase("ie-cloud")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browser", "IE");
+            caps.setCapability("browser_version", "11.0");
+            caps.setCapability("os", "Windows");
+            caps.setCapability("os_version", "10");
+            caps.setCapability("resolution", "1280x1024");
+        }
+        if (browser.equalsIgnoreCase("chrome")){
+            ChromeDriverManager.getInstance().setup();
+            driver=new ChromeDriver();
+        }else if(browser.equalsIgnoreCase("FireFox")){
+            FirefoxDriverManager.getInstance().setup();
+            driver=new FirefoxDriver();
+        }
+
+    }
+    public ApplicationController wholeFood(){
+        if (wholeFood==null){
+            wholeFood=new ApplicationController(driver);
+
+        }return wholeFood;
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+        if ((driver!=null)){
+            driver.close();;
+        }
+        try {
+            driver.quit();
+        }catch (Exception ex){
+        }
+        wholeFood=null;
+    }
+}
